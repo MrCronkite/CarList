5c8043549018ca978cd05d588b4fdb5876f5b261
MrCronkite <vladshima99@gmail.com>
2023-12-26 13:09:29 +0300

    Initial Commit

---
+++ CarList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarList
+//
+//  Created by Vladislav Shimchenko on 26.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
